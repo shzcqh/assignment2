aac259ebddd76bf3380e7c2c29373cffa584d6cb
unknown <2867459421@qq.com>
2025-04-17 08:41:53 +0800

    add S3 bucket, DynamoDB table, SQS queue & DLQ

--- lib/assignment2-stack.ts
+++ lib/assignment2-stack.ts
@@ -1,16 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3        from 'aws-cdk-lib/aws-s3';
+import * as dynamodb  from 'aws-cdk-lib/aws-dynamodb';
+import * as sqs       from 'aws-cdk-lib/aws-sqs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Assignment2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+// ---------- S3 Bucket: Save photos ----------
+const photosBucket = new s3.Bucket(this, 'PhotosBucket', {
+  removalPolicy: cdk.RemovalPolicy.DESTROY,     
+  autoDeleteObjects: true,                      
+});
+    // ---------- DynamoDB: Keep a record of the pictures ----------
+const imagesTable = new dynamodb.Table(this, 'ImagesTable', {
+  partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+  removalPolicy: cdk.RemovalPolicy.DESTROY,      
+});
 
-    // The code that defines your stack goes here
+// ---------- SQS:Dead-letter queues  ----------
+const badMessagesQueue = new sqs.Queue(this, 'BadMessagesQueue', {
+  retentionPeriod: cdk.Duration.days(14),
+});
+// ---------- SQS The main queue is used to receive upload events ----------
+const uploadsQueue = new sqs.Queue(this, 'UploadsQueue', {
+  deadLetterQueue: {
+    queue: badMessagesQueue,
+    maxReceiveCount: 2,      
+  },
+  visibilityTimeout: cdk.Duration.seconds(60),
+});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Assignment2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+// ---------- Export the name of the resource ----------
+new cdk.CfnOutput(this, 'BucketName', { value: photosBucket.bucketName });
+new cdk.CfnOutput(this, 'TableName',  { value: imagesTable.tableName  });
+new cdk.CfnOutput(this, 'QueueURL',   { value: uploadsQueue.queueUrl  });
   }
 }
